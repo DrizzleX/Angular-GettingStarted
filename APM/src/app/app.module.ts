fdf1e6ee3e1a7da3ccaba1f8aa33283eff950487
Theo Si <theosi@outlook.com>
2019-04-18 08:59:24 +0800

    add product.ts and css for product-list

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NameListComponent } from './namelist.component';
@@ -11,7 +12,7 @@ import { ProductListComponent } from './products/product-list.component';
     AppComponent, NameListComponent, ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   bootstrap: [AppComponent]
 })
